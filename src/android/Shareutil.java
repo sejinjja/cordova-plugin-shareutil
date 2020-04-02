69347a22ef6117a84f30f1f8bfac9808d3ae37ff
sejin <sejinjja@bpnr.co.kr>
2020-04-02 09:01:13 +0900

    코드 포매팅.

--- src/android/Shareutil.java
+++ src/android/Shareutil.java
@@ -9,46 +9,41 @@ import org.json.JSONException;
 import android.content.Intent;
 
 public class Shareutil extends CordovaPlugin {
+	@Override
+	public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+		if ("shareText".equals(action)) {
+			String text = data.getString(0);
+		} else if ("shareImg".equals(action)) {
+			String base64 = data.getString(0);
+			String mimeType = data.getString(1);
+		} else {
+			return false;
+		}
+	}
 
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-        if("shareText".equals(action)) {
-            String text = data.getString(0);
-        }else if("shareImg".equals(action)) {
-            String base64 = data.getString(0);
-            String mimeType = data.getString(1);
-        } else {
-            return false;
-        }
-    }
+	private void share(String text, CallbackContext callbackContext) {
+		try {
+			Intent sendIntent = new Intent();
+			sendIntent.setAction(Intent.ACTION_SEND);
+			sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+			sendIntent.setType("plain/text");
+			cordova.getActivity().startActivity(Intent.createChooser(sendIntent, text));
+			callbackContext.success();
+		} catch (Error e) {
+			callbackContext.error(e.getMessage());
+		}
+	}
 
-
-    private void share(String text, CallbackContext callbackContext) {
-      try {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, text);
-        sendIntent.setType("plain/text");
-        cordova.getActivity().startActivity(Intent.createChooser(sendIntent, text));
-        callbackContext.success();
-        } catch(Error e) {
-            callbackContext.error(e.getMessage());
-        }
-
-    }
-
-
-    private void share(String base64, String mimeType, CallbackContext callbackContext) {
-      try {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_STREAM, base64);
-        sendIntent.setType(mimeType);
-        cordova.getActivity().startActivity(Intent.createChooser(sendIntent, "Share Image"));
-        callbackContext.success();
-        } catch(Error e) {
-            callbackContext.error(e.getMessage());
-        }
-
-    }
+	private void share(String base64, String mimeType, CallbackContext callbackContext) {
+		try {
+			Intent sendIntent = new Intent();
+			sendIntent.setAction(Intent.ACTION_SEND);
+			sendIntent.putExtra(Intent.EXTRA_STREAM, base64);
+			sendIntent.setType(mimeType);
+			cordova.getActivity().startActivity(Intent.createChooser(sendIntent, "Share Image"));
+			callbackContext.success();
+		} catch (Error e) {
+			callbackContext.error(e.getMessage());
+		}
+	}
 }
