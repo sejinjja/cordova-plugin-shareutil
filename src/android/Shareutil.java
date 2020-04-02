20bc9e4608e0a5dcc8df3612d385f532befb0fd3
sejin <sejinjja@bpnr.co.kr>
2020-04-02 10:41:49 +0900

    base64 to imageURI 처리 추가.

--- src/android/Shareutil.java
+++ src/android/Shareutil.java
@@ -1,5 +1,9 @@
 package kr.sejiwork.cordova.shareutil;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -9,7 +13,9 @@ import org.json.JSONException;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
 public class Shareutil extends CordovaPlugin {
@@ -43,9 +49,10 @@ public class Shareutil extends CordovaPlugin {
 
 	private void share(String base64, String mimeType, CallbackContext callbackContext) {
 		try {
+		  File decodedBase64File = this.saveImage(cordova.getActivity().getApplicationContext(), base64)
 			Intent sendIntent = new Intent();
 			sendIntent.setAction(Intent.ACTION_SEND);
-			sendIntent.putExtra(Intent.EXTRA_STREAM, base64);
+			sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(decodedBase64File));
 			sendIntent.setType(mimeType);
 			cordova.getActivity().startActivity(Intent.createChooser(sendIntent, "Share Image"));
 			callbackContext.success();
@@ -53,6 +60,34 @@ public class Shareutil extends CordovaPlugin {
 			callbackContext.error(getPrintStackTrace(e));
 		}
 	}
+
+	private static File saveImage(final Context context, final String imageData) {
+      final byte[] imgBytesData = android.util.Base64.decode(imageData, android.util.Base64.DEFAULT);
+
+      final File file = File.createTempFile("tempImageForShare", null, context.getCacheDir());
+      final FileOutputStream fileOutputStream;
+      try {
+          fileOutputStream = new FileOutputStream(file);
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+          return null;
+      }
+
+      final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+      try {
+          bufferedOutputStream.write(imgBytesData);
+      } catch (IOException e) {
+          e.printStackTrace();
+          return null;
+      } finally {
+          try {
+              bufferedOutputStream.close();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+      }
+      return file;
+  }
 
 	private static String getPrintStackTrace(Exception e) {
 		StringWriter errors = new StringWriter();
